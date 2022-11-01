200c6f021f10470e0ccfc55a61b84f86231b881c
OddMattHatt3r <67079190+OddMattHatt3r@users.noreply.github.com>
2022-11-01 00:27:45 -0400

    Update ArrayPrint.java

--- ArrayPrint.java
+++ ArrayPrint.java
@@ -1,26 +1,45 @@
-  // Import the IOException class to handle errors
+import java.io.File;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-public class Main{
+public class AppTest{
     public static void main(String[] args) {
 
-        String[] array = {"Apple","Banana","Orange","Apple","Peanut","Banana","Orange","Apple", "Peanut", "Peanut"};
+        //String[] cities = {"union", "union", "piscataway"};
+
+
+        try{
+            File myObj = new File("SearchSummary.txt");
+            if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists, information updated");
+        }
+
+        String[] array = {"Apple","Banana","Apple","Peanut","Banana","Orange","Apple", "Peanut", "Peanut"};
 
         Map<String, Long> map = Stream.of(array).collect(Collectors.
-            groupingBy(Function.identity(), //use groupingBy array element
+         groupingBy(Function.identity(), //use groupingBy array element
                  Collectors.counting())); //count number of occurances
         //System.out.println(map);//output the results of the Map
 
         String x = map.toString();
         x = x.replaceAll(",","");
         String str = x.substring(1, x.length() - 1);
-        str = str.replaceAll("=",":");
+        str = str.replaceAll("=","(");
+        str = str.replaceAll(" ", " times) ");
 
-        System.out.println("\n"+ str);
-
-        
+        FileWriter myWriter = new FileWriter("SearchSummary.txt");
+            myWriter.write("Number of times each city was searched: " + str +" times)");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
 
     }
   }
+}
