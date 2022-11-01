c276d45fcd69bb44df8fee828f0b6e975e147336
OddMattHatt3r <67079190+OddMattHatt3r@users.noreply.github.com>
2022-10-31 20:17:26 -0400

    Update PracticeFileWriting.java

--- PracticeFileWriting.java
+++ PracticeFileWriting.java
@@ -1,15 +1,23 @@
+import java.io.File;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
-public class PracticeFileWriting{
+public class AppTest{
     public static void main(String[] args) {
-        try {
-            FileWriter myWriter = new FileWriter("SearchSummary.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+        try{
+            File myObj = new File("SearchSummary.txt");
+            if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists, information updated");
+        }
+        FileWriter myWriter = new FileWriter("SearchSummary.txt");
+            myWriter.write("Files in Java might be tricky, but it is simple enough!");
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+
     }
   }
 }
