8723387c6d9736d3aa1453d71873295a12c7799d
OddMattHatt3r <67079190+OddMattHatt3r@users.noreply.github.com>
2022-11-01 10:14:23 -0400

    Update and rename ArrayPrint.java to ListPrint.java

--- ListPrint.java
+++ ListPrint.java
@@ -1,14 +1,23 @@
 import java.io.File;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 public class AppTest{
     public static void main(String[] args) {
 
         //String[] cities = {"union", "union", "piscataway"};
+
+        ArrayList<String> cities = new ArrayList<String>();
+
+        cities.add("Union");
+        cities.add("Jersey City");
+        cities.add("Union");
+        cities.add("Piscataway");
+        cities.add("Union");
+        cities.add("Piscataway");
+        cities.add("Union");
 
 
         try{
@@ -19,21 +28,16 @@ public class AppTest{
             System.out.println("File already exists, information updated");
         }
 
-        String[] array = {"Apple","Banana","Apple","Peanut","Banana","Orange","Apple", "Peanut", "Peanut"};
+        Map<String, Long> counts =
+        cities.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
 
-        Map<String, Long> map = Stream.of(array).collect(Collectors.
-         groupingBy(Function.identity(), //use groupingBy array element
-                 Collectors.counting())); //count number of occurances
-        //System.out.println(map);//output the results of the Map
-
-        String x = map.toString();
-        x = x.replaceAll(",","");
+        String x = counts.toString();
+        x = x.replaceAll(","," times, ");
         String str = x.substring(1, x.length() - 1);
-        str = str.replaceAll("=","(");
-        str = str.replaceAll(" ", " times) ");
+        str = str.replaceAll("="," - ");
 
         FileWriter myWriter = new FileWriter("SearchSummary.txt");
-            myWriter.write("Number of times each city was searched: " + str +" times)");
+            myWriter.write("Number of times each city was searched: " + str +" times");
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
